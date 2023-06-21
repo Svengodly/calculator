a1f67d2d9a5491626c2552b640468f94cf741ee0
Rickey Morris <rmorris.rickey@gmail.com>
2023-06-21 19:08:52 -0400

    The subtraction assignment operators were swapped for an equal sign in the subtract method. This was due to a misunderstanding of how the -= works, and resulted in unexpected results. Example: 3 -= 4 - 5 evaluates to 4. I thought the answer would be -6 (3 - 4 - 5), but the expression right of the operand (4 - 5) is evaluated first. This results in 3 - (-1), which is evaluates to 4.

--- myScript.js
+++ myScript.js
@@ -1,26 +1,34 @@
+//Arithmetic assignments (-=, +=, etc) will evaluate the right side of the equation first before using the operand left of the equation.
 function add(number1, number2){
     //The arguments are still strings, so they need to be converted to numbers for calculations.
-    total += Number.parseInt(number1) + Number.parseInt(number2);
+    if (total == undefined)
+    {
+        total = Number.parseInt(number1) + Number.parseInt(number2);
+    }
+    total = total + Number.parseInt(number1) + Number.parseInt(number2);
 return total;
 }
 
-function subtract(...number){
-    let sum = number[0];
-    for (let value of number) {
-        sum -= value;
+function subtract(number1, number2){
+    if (total == undefined){
+        total = Number.parseInt(number1) - Number.parseInt(number2);
     }
-return sum;
+    else {
+        total = total - Number.parseInt(number1) - Number.parseInt(number2);
+        console.log(total);
+    }
+return total;
 }
 
-function multiply(...number){
-    let sum = 0;
-    for (let value of number){
-        sum *= value
+function multiply(number1, number2){
+    if (total == undefined){
+        total = Number.parseInt(number1) * Number.parseInt(number2);
     }
-return sum;
+    total = total * Number.parseInt(number1) * Number.parseInt(number2);
+return total;
 }
 
-function divide(...number){
+function divide(number1, number2){
     //If the number is used as the divisor, check for zero.
     if (number == 0){
         return console.log("Cannot divide by zero!");
@@ -31,9 +39,11 @@ return;
 
 function getNumber(numberPressed){
     console.log("Twas a number!")
-    if (userNumber1 && operand != undefined){
-        userNumber2 += numberPressed;  
-        add(userNumber1, userNumber2); 
+    //The Number.isInteger and Number.parseInt is used to deal with 0 being a falsy value.
+    if (Number.isInteger(Number.parseInt(userNumber1)) && operand){
+        userNumber2 += numberPressed;
+        //Need to make the code determine whether to perform addition, subtraction, multiplication, or division.  
+        operate(operand, userNumber1, userNumber2); 
     }
     else {
         userNumber1 += numberPressed;
@@ -42,7 +52,7 @@ function getNumber(numberPressed){
 
 function getKey(keyDetails){
     console.log(keyDetails);
-    if (Number.parseInt(keyDetails.key)){
+    if (Number.isInteger(Number.parseInt(keyDetails.key))){
         getNumber(keyDetails.key);
     }
     //If keyDetails.key is not an integer and userNumber has a value, perform a switch case to determine what operand should be set to.
@@ -66,16 +76,34 @@ function getKey(keyDetails){
     }
 }
 
+function operate(operand, number1, number2){
+    switch(operand){
+        case "+":
+            add(number1, number2);
+            break;
+        case "-":
+            subtract(number1, number2);
+            break;
+        case "*":
+            multiply(number1, number2);
+            break;
+        case "/":
+            divide(number1, number2);
+            break;
+        default:
+            console.log("Error");
+    }
+}
 //Going to need a function that resets the values provided by the user.
 // function clear()
 
-//Global variable that can be accessed by each of the functions and keeps track of the total sum: "total"
+//Global variable that can be accessed by each of the functions and keeps track of the total total: "total"
 let total = null;
 
 //Listen for keyboard inputs to retrieve and store numbers and operand. Initializing userNumber variables to "" to perform string concatenation with key values retreived from keydown event
 //, which are strings.
 let userNumber1 = "";
 let userNumber2 = "";
-let operand;
+let operand = null;
 
 document.addEventListener("keydown", getKey);
