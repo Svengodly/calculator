e4715d950eb898e51bf473d973d8b22f41c0c639
Rickey Morris <rmorris.rickey@gmail.com>
2023-07-02 18:24:24 -0400

    Made several changes to 'myScript.js.' Swapped all instances of Number.isInteger with isNaN and changed Number.parseInt to Number.parseFloat. Removed the if (total || total == 0) conditional lines in the 'setOperand' function. Added RegEx.test() in 'updateDisplay'function that checks for operands.

--- myScript.js
+++ myScript.js
@@ -1,15 +1,25 @@
 //Arithmetic assignments (-=, +=, etc) will evaluate the right side of the equation first before using the operand left of the equation.
 function add(number1, number2){
     //The arguments are still strings, so they need to be converted to numbers for calculations.
-        return total = Number.parseInt(number1) + Number.parseInt(number2);
+        displayNum = Number.parseFloat(number1) + Number.parseFloat(number2);
+        //Clear out 'operand' and 'nextNumber' for further entries.
+        operand = "";
+        nextNumber = "";
+        return total = Number.parseFloat(number1) + Number.parseFloat(number2);
     }
 
 function subtract(number1, number2){
+        displayNum = number1 - number2;
+        operand = "";
+        nextNumber = "";
         return total = number1 - number2;
     }
 
 function multiply(number1, number2){
-        return total = Number.parseInt(number1) * Number.parseInt(number2);
+        displayNum = number1 * number2;
+        operand = "";
+        nextNumber = "";
+        return total = number1 * number2;
     }
 
 function divide(number1, number2){
@@ -19,53 +29,39 @@ function divide(number1, number2){
         return display.textContent = "What the f#@#!";
     }
     else {
+        displayNum = number1 / number2;
+        operand = "";
+        nextNumber = "";
         return total = number1 / number2;
     }
     //else divide some stored value by "number."
 }
 
 function getNumber(numberSelected){
-    if(Number.isInteger(Number.parseInt(userNumber1)) && operand){
-        userNumber2 += numberSelected;
-        display.textContent= operate(operand, userNumber1, userNumber2); 
+    if(!isNaN(firstNumber) && operand){
+        nextNumber += numberSelected;
+        displayNum = nextNumber;
+        display.textContent = displayNum; 
     }
     else {
-        userNumber1 += numberSelected;
+        firstNumber == 0 ? firstNumber = numberSelected : firstNumber += numberSelected;
     }
-    console.log(userNumber1, userNumber2);
+    console.log(firstNumber, nextNumber);
 }
 
 function setOperand(pressedKey){
     switch(pressedKey){
         case "+":
             operand = "+";
-            if (total || total == 0){
-                //Retain the result of the displayed value and the most recent value used in the eqaution.
-                userNumber1 = total;
-                userNumber2 = "";
-            };
             break;
         case "-":
             operand = "-";
-            if (total || total == 0){
-                // displayNum = displayNum - userNumber2;
-                userNumber1 = total;
-                userNumber2 = "";
-            }
             break;
         case "*":
             operand = "*";
-            if (total || total == 0){
-                userNumber1 = total;
-                userNumber2 = "";
-            };
             break;
         case "/":
             operand = "/";
-            if (total || total == 0){
-                userNumber1 = total;
-                userNumber2 = "";
-            };
             break;
         default:
             console.log("No matches.");
@@ -74,11 +70,11 @@ function setOperand(pressedKey){
 
 function getKey(keyDetails){
     console.log(keyDetails);
-    if (Number.isInteger(Number.parseInt(keyDetails.key))){
+    if (!isNaN(keyDetails.key)){
         getNumber(keyDetails.key);
     }
     //If keyDetails.key is not an integer and userNumber has a value, perform a switch case to determine what operand should be set to via the setOperand function.
-    else if (userNumber1 || userNumber1 == 0){
+    else if (firstNumber || firstNumber == 0){
         setOperand(keyDetails.key);
     }
 }
@@ -86,13 +82,13 @@ function getKey(keyDetails){
 function operate(operand, number1, number2){
     switch(operand){
         case "+":
-            return add(number1, number2);
+            return display.textContent = add(number1, number2);
         case "-":
-            return subtract(number1, number2);
+            return display.textContent = subtract(number1, number2);
         case "*":
-            return multiply(number1, number2);
+            return display.textContent = multiply(number1, number2);
         case "/":
-            return divide(number1, number2);
+            return display.textContent = divide(number1, number2);
         default:
             console.log("Error");
     }
@@ -101,30 +97,38 @@ function operate(operand, number1, number2){
 //Update display when a button is clicked
 function updateDisplay(buttonClicked){
     console.log(buttonClicked.target.innerText);
-    if (displayNum == 0 && buttonClicked.target.innerText == 0){
-        return;
-    }
-    else if (Number.isInteger(Number.parseInt(buttonClicked.target.innerText))){
+    if (!isNaN(buttonClicked.target.innerText)){
         //A calculator's display will show the number "0" on startup and will not change if the number "0" is selected on the keypad.
         //This checks to see if the display number is "0." If it is, set the display to whatever number is clicked. 
         displayNum == 0 ? displayNum = buttonClicked.target.innerText : displayNum += buttonClicked.target.innerText;
+        getNumber(buttonClicked.target.innerText);
         return display.textContent = displayNum;
+    }
+    else if (/[+*/=-]/.test(buttonClicked.target.innerText) && firstNumber){
+        console.log("You Chose: ", buttonClicked.target.innerText);
+        if (buttonClicked.target.innerText == "=" && operand && firstNumber && nextNumber) {
+            //For the second arguement: If 'total' has a value, then use that in lieu of 'firstNumber' which does not change after a value is assigned to it or if it's cleared with 'clear()'. 
+            operate(operand, (total != null && !isNaN(total)) ? total : firstNumber, nextNumber);
+        }
+        else {
+            setOperand(buttonClicked.target.innerText);
+        }
     }
 }
 
 //Going to need a function that resets the values provided by the user.
 // function clear()
 
-//Global variable that can be accessed by each of the functions and keeps track of the total total: "total"
-let total = "0";
+//Global variable that can be accessed by each of the functions and keeps track of the total.
+let total = null;
 
 //Track intermediate values.
 let displayNum = "0";
 
 //Listen for keyboard inputs to retrieve and store numbers and operand. Initializing userNumber variables to "" to perform string concatenation with key values retreived from keydown event
 //, which are strings.
-let userNumber1 = "";
-let userNumber2 = "";
+let firstNumber = "";
+let nextNumber = "";
 let operand = null;
 
 document.addEventListener("keydown", getKey);
