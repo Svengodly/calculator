2601113bf85bce7b23892bee00e7cdeff5162223
Rickey Morris <rmorris.rickey@gmail.com>
2023-07-04 15:57:07 -0400

    Added lines to 'divide' function that resets 'nextNumber' to an empty string. This prevents a bug where further digits would be appended to the first number. The '.' now becomes disalbed if either the first or any subsequent numbers have a '.' in them. Added 'clear' function, which resets all variables to their original values. This is applied to the 'C' button.

--- myScript.js
+++ myScript.js
@@ -1,3 +1,6 @@
+// TODO: Determine how to deal with floating point numbers (floating-point-gui). For example, the subtraction of 5.2 - 1.5 yields 3.7 as expected. However 5.2 - 1.4 yields 3.800000003.
+// TODO: Find a way to deal with answers that are larger than the display window.
+
 //Arithmetic assignments (-=, +=, etc) will evaluate the right side of the equation first before using the operand left of the equation.
 function add(number1, number2){
     //The arguments are still strings, so they need to be converted to numbers for calculations.
@@ -26,7 +29,8 @@ function divide(number1, number2){
     //If the number is used as the divisor, check for zero.
     if (number2 == 0){
         console.log("Cannot divide by zero!");
-        return display.textContent = "What the f#@#!";
+        nextNumber = "";
+        return display.textContent = "(x_x) no / 0";
     }
     else {
         displayNum = number1 / number2;
@@ -39,12 +43,15 @@ function divide(number1, number2){
 
 function getNumber(numberSelected){
     if(!isNaN(firstNumber) && operand){
-        nextNumber += numberSelected;
+        if (!nextNumber.includes(".")){
+        dotButton.removeAttribute("disabled");
+        }
+        nextNumber == "0" ? nextNumber = numberSelected : nextNumber += numberSelected;
         displayNum = nextNumber;
         display.textContent = displayNum; 
     }
     else {
-        firstNumber == 0 ? firstNumber = numberSelected : firstNumber += numberSelected;
+        firstNumber == "0" ? firstNumber = numberSelected : firstNumber += numberSelected;
     }
     console.log(firstNumber, nextNumber);
 }
@@ -80,6 +87,7 @@ function getKey(keyDetails){
 }
 
 function operate(operand, number1, number2){
+    dotButton.setAttribute("disabled", true);
     switch(operand){
         case "+":
             return display.textContent = add(number1, number2);
@@ -96,19 +104,41 @@ function operate(operand, number1, number2){
 
 //Update display when a button is clicked
 function updateDisplay(buttonClicked){
-    console.log(buttonClicked.target.innerText);
+    // console.log(buttonClicked.target.innerText);
+    // Checks 'clientWidth' and 'scrollWidth' HTML DOM Element properties as a way to restrict the amount of characters in the display by comparing the width of the div relative to its content.
+   
     if (!isNaN(buttonClicked.target.innerText)){
+        if (document.querySelector("#display").clientWidth < document.querySelector("#display").scrollWidth && !operand){
+            console.log("It's too much!!!");
+            return;
+        }
+    
         //A calculator's display will show the number "0" on startup and will not change if the number "0" is selected on the keypad.
         //This checks to see if the display number is "0." If it is, set the display to whatever number is clicked. 
-        displayNum == 0 ? displayNum = buttonClicked.target.innerText : displayNum += buttonClicked.target.innerText;
+        //displayNum and others were changed from the digit 0 to the string '0' because '0' == '0.' is true but 0 == '0.' is false.
+        displayNum == "0" ? displayNum = buttonClicked.target.innerText : displayNum += buttonClicked.target.innerText;
         getNumber(buttonClicked.target.innerText);
         return display.textContent = displayNum;
     }
-    else if (/[+*/=-]/.test(buttonClicked.target.innerText) && firstNumber){
-        console.log("You Chose: ", buttonClicked.target.innerText);
+    else if (/[-+*/=\\.]/.test(buttonClicked.target.innerText) && firstNumber){
         if (buttonClicked.target.innerText == "=" && operand && firstNumber && nextNumber) {
             //For the second arguement: If 'total' has a value, then use that in lieu of 'firstNumber' which does not change after a value is assigned to it or if it's cleared with 'clear()'. 
             operate(operand, (total != null && !isNaN(total)) ? total : firstNumber, nextNumber);
+        }
+        else if (buttonClicked.target.innerText == "." && (firstNumber || total == 0))
+        {
+            if (!nextNumber){
+                firstNumber += ".";
+                dotButton.setAttribute("disabled", true);
+                displayNum = firstNumber;
+                display.textContent = displayNum;
+            }
+            else {
+                nextNumber+= ".";
+                dotButton.setAttribute("disabled", true);
+                displayNum = nextNumber;
+                display.textContent = displayNum;   
+            }
         }
         else {
             setOperand(buttonClicked.target.innerText);
@@ -116,10 +146,18 @@ function updateDisplay(buttonClicked){
     }
 }
 
-//Going to need a function that resets the values provided by the user.
-// function clear()
+//Resets calculator's variables to their inital values. Similar to hitting the 'C,' or Clear, button on a calculator. 
+function clear(){
+    total = null;
+    displayNum = "0";
+    display.textContent = displayNum;
+    firstNumber = "";
+    nextNumber = "";
+    operand = null;
+    dotButton.removeAttribute("disabled");
+}
 
-//Global variable that can be accessed by each of the functions and keeps track of the total.
+//Variable that keeps track of the total.
 let total = null;
 
 //Track intermediate values.
@@ -131,14 +169,19 @@ let firstNumber = "";
 let nextNumber = "";
 let operand = null;
 
+// Button for '.'
+const dotButton = document.querySelector("#numpad").lastElementChild.firstElementChild.nextElementSibling;
+
+//Add 'keydown' event listener which then runs the 'getKey' function.
 document.addEventListener("keydown", getKey);
-// const display = document.createElement("p");
-// const display = document.querySelector("#display").textContent = total;
+
 display.textContent = displayNum;
-// document.body.appendChild(display);
 
 // Add event listeners to all buttons
 const numberButtons = document.querySelectorAll(".number button");
+
+//Add event listener to the 'C' button.
+document.querySelector("#clear button").addEventListener("click", clear);
 
 numberButtons.forEach((button) => {
     button.addEventListener("click", updateDisplay);
